04321f37c502df75834bdea4cd8fb22fca4452bd
Tomasz Suchwałko <tomasz.suchwalko@cssoftware.pl>
2018-04-03 16:23:51 +0200

    authentication done

--- src/webserviceclient/client1.java
+++ src/webserviceclient/client1.java
@@ -5,6 +5,13 @@
  */
 package webserviceclient;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.handler.MessageContext;
 import mainpackage.IHelloDarknessMyOldFriend;
 import mainpackage.HelloDarknessMyOldFriendService;
 
@@ -13,16 +20,40 @@ import mainpackage.HelloDarknessMyOldFriendService;
  * @author Slightom
  */
 public class client1 {
-    public static void main(String[] args){
+
+    private static final String WS_URL = "http://localhost:9999/ws3/hellodarkness?wsdl";
+
+    public static void main(String[] args) {
         HelloDarknessMyOldFriendService helloService = new HelloDarknessMyOldFriendService();
         IHelloDarknessMyOldFriend hello = helloService.getHelloDarknessMyOldFriendPort();
-        
-        String zapytanie = "Jacob";
-        String response = hello.getString(zapytanie);
-        System.out.println("Client1: \n" +
-                "Klient wysłał: " + zapytanie + "\n" +
-                "Klient otrzymał: " + response);
-        
-        
+        String userName = "", password = "", response = "";
+
+        while (response.isEmpty() || response.contains("Unknown User!")) {
+            /*
+            * *****************UserName & Password*****************************
+             */
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Please enter username: ");
+            userName = scan.next();
+            System.out.print("Please enter password: ");
+            password = scan.next();
+
+            Map<String, Object> req_ctx = ((BindingProvider) hello).getRequestContext();
+            req_ctx.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, WS_URL);
+
+            Map<String, List<String>> headers = new HashMap<String, List<String>>();
+            headers.put("Username", Collections.singletonList(userName));
+            headers.put("Password", Collections.singletonList(password));
+            req_ctx.put(MessageContext.HTTP_REQUEST_HEADERS, headers);
+            /*
+            ********************************************************************
+             */
+
+            response = hello.getString(userName);
+            System.out.println("Client1: \n"
+                    + "Klient wysłał: " + userName + "\n"
+                    + "Klient otrzymał: " + response);
+        }
+
     }
 }
